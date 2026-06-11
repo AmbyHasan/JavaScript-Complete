ac82f82aac713e1821828787b15f1c26046c6d36
Amber Hasan <Amber.Hasan@Geminisolutions.com>
2026-06-11 11:17:57 +0530

    Pushing code

--- JS_PART-1/variables_and_declaration.js
+++ JS_PART-1/variables_and_declaration.js
@@ -3,21 +3,30 @@
 //js ko brenden eek ne 15 days me banaya tha, 
 //ecmascript community manages js
 
-//var is the old way to declare a variable, it is function scoped and can be re-declared and updated
-var a="Amber";
-//or 
-var a;   //declaration
-a="Amber";  //initialization
-//it has been re declared and error nahi aya
+// var and let ko notes se hi padh lo
 
-//now days we use let and const to declare variables, let is block scoped and can be updated but not re-declared, const is block scoped and cannot be updated or re-declared
-let a="Amber";
-//or
-let a;   //declaration
-a="Amber";  //initialization
+//const is used when we want to declare a variable whose value will not change, it is a constant variable. once we assign a value to a const variable, we cannot reassign it. it is block scoped and must be initialized at the time of declaration. if we try to reassign a const variable, it will throw an error. const variables are often used for values that should remain constant throughout the program, such as mathematical constants or configuration settings.
+//but if a const containa an object or array , still its value can be changes because the reference to the object or array is constant, but the properties of the object or elements of the array can be modified. for example:
+const myArray = [1, 2, 3];
+myArray.push(4);  //valid
 
-const a="Amber"; //declaration and initialization at the same time, it cannot be updated or re-declared
+const myObj={name: "Amber"};
+myObj.name="Aman"; //valid
 
-//hoisting is a behavior in js where variable and function declarations are moved to the top of their scope before code execution, but only the declarations are hoisted, not the initializations
-console.log(a); //undefined
-var a="Amber"; //declaration and initialization
+
+//Dataypes=>
+//null -> apne jaan boojh ke variable khali rakha hai, kisi variable ko null assign karne ka matlab hai ki usme koi value nahi hai, lekin uska data type abhi bhi object hai. 
+//undefined-> apne koi variable declare kiya hai but koi value nhi di to jo value by default mili that is undefined
+
+//typeOf
+
+typeof null; //object - >historical bug hai bhai , known bug hai .. sabko pata hai bug hai but tab bhi yhi chal rha haii
+
+console.log(typeof []);
+console.log(typeof null);
+console.log(typeof 123n);
+
+
+
+console.log(5!=='5'); //true -> because here we have strict checking
+console.log(5!='5'); //false -> becuse here we have loose checking, in loose checking js will convert the string '5' to number 5 and then compare, so it will be equal and return false. but in strict checking it will not convert and directly compare the values and types, so it will return true because 5 is a number and '5' is a string.
