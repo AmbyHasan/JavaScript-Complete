db0c2ffbfd03e0bc14ab4e0be06d4cb1d90dca43
Amber Hasan <Amber.Hasan@Geminisolutions.com>
2026-06-11 15:30:35 +0530

    Push

---
+++ JS_PART-2/EventListners/script.js
@@ -0,0 +1,4 @@
+//Browser me page par koi bhi harkat karo to ek event raise ho jayega
+//yani kuch bhi ho jaye screen par to wo ek event hai ..
+//event ka matlab hai action hua , eventListener ka matlaba hai apne us action ka reaction diya
+
