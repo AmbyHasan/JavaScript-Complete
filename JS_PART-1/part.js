37402fbd36b48765d3b656170094480da9527eb4
Amber Hasan <Amber.Hasan@Geminisolutions.com>
2026-06-11 14:03:29 +0530

    Pushing the part 1

--- JS_PART-1/part.js
+++ JS_PART-1/part.js
@@ -31,6 +31,60 @@ console.log(typeof 123n);
 console.log(5!=='5'); //true -> because here we have strict checking
 console.log(5!='5'); //false -> becuse here we have loose checking, in loose checking js will convert the string '5' to number 5 and then compare, so it will be equal and return false. but in strict checking it will not convert and directly compare the values and types, so it will return true because 5 is a number and '5' is a string.
 
+
+//For converting any value to boolean
 console.log(!!3); //this is a trick to convert any value to boolean , it will give true
 let a=10;
 console.log(!!a); //it will also give true because a is a truthy value, any non zero number is a truthy value in js, so it will return true. if we have a=0 then it will return false because 0 is a falsy value in js.
+console.log(!!"Amber");//true
+
+
+//For converting any value to number
+let str = "42";
+let num = +str;
+console.log(num); // 42
+
+
+//loops
+for(let char of "Amber"){
+    console.log(char);
+}
+
+let nums=[10,20,30,40];
+nums.forEach((num)=>{
+    console.log(num);
+})
+
+
+//HIGHER ORDER FUNCTIONS -> Wo function ho ya to function ko accept karte ho ya return karte ho
+ function abc(val){
+    return function(){
+        console.log(val);
+    }
+ }
+
+ abc(10)(); //dusra bracket isliye lagaya hai kyuki abc(10) ek function return karega to usko call karne ke liye dusra bracket lagana padega, so it will print 10
+
+
+ //IIFE
+
+ //turant function chal jayega
+
+ (function(){
+console.log("This is an IIFE");
+ })()
+
+
+ //HOISTING
+
+ abcd();
+
+ function abcd(){
+    console.log("This is a hoisted function");
+ }
+
+ //ye chal jayega
+ //function expression me hoisting possible nhi hoti hai   
+
+ abcd2(); //ye error dega because abcd2 is a function expression and it is not hoisted, so we cannot call it before its declaration, it will throw a ReferenceError: Cannot access 'abcd2' before initialization.
+ let abcd2 = function(){  console.log("This is a function expression");}
